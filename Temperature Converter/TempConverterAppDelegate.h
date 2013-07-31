7c9a86529a349a49c654de8b7707b52a620aca52
Kevin Patel <kevin36524@gmail.com>
2013-07-31 17:19:34 +0530

    Initial Commit

---
+++ Temperature Converter/TempConverterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TempConverterAppDelegate.h
+//  Temperature Converter
+//
+//  Created by Kevin Patel on 7/31/13.
+//  Copyright (c) 2013 Kevin Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TempConverterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Temperature Converter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Temperature Converter
+//
+//  Created by Kevin Patel on 7/31/13.
+//  Copyright (c) 2013 Kevin Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TempConverterAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TempConverterAppDelegate class]));
+    }
+}
